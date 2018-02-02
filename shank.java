fb3bc36fc179d112c9b28a2d306b6a2aa8c14166
savaughn <savaughn@uark.edu>
2018-02-02 12:39:55 -0600

    attempt at list2 build & compare bigints in arraylist

--- shank.java
+++ shank.java
@@ -24,12 +24,39 @@ class Shank{
 
 	void solve(BigInteger a, BigInteger b, BigInteger p, BigInteger n){
 		
+		BigInteger tmp = new BigInteger();
+		
 		//List1   e, g, g2, g3, . . . , gn  
 		for(BigInteger i = n; i.compareTo(BigInteger.ZERO) > 0; i = i.subtract(BigInteger.ONE)){
 			list1.add(a.modPow(i,p));
+			list2.add(b.multiply(a.modPow(n.multiply(i),p)));
 		}		
 		System.out.println(list1);
-			
+		System.out.println(list2);
+		
+		/*
+		
+		for(tmp : list1){
+			if(list2.contains(tmp)){
+				System.out.println("found");
+			}
+		}
+		
+		
+		for(int i = n.intValue(); i > 0; i--){
+			for(int j = n.intValue(); j > 0; j--){
+				if(list1[i].equals(list2[j])){
+					System.out.println("found");
+				}
+			}
+		}
+		
+		for(BigInteger i = n; i.compareTo(BigInteger.ZERO) > 0; i = i.subtract(BigInteger.ONE)){
+			for(BigInteger j = n; j.compareTo(BigInteger.ZERO) > 0; j = j.subtract(BigInteger.ONE)){
+				if (list1[i] == list2[j])
+					System.out.println("x=");
+			}
+		}*/
 		
 		//TODO: List2 h*g^(-in) mod p		
 		//TODO: compare both lists for match
