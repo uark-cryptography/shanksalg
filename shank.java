e1c40c0c7dc1a000c50a00ae5859f7ecaa13e5b0
savaughn <savaughn@uark.edu>
2018-02-01 21:41:31 -0600

    initial commit

---
+++ shank.java
@@ -0,0 +1,20 @@
+class Shank{
+
+	void solve(double a, double, b, double p){
+		
+	}
+
+	public static void main(String args[]){
+		
+		Shank s = new Shank();
+		
+		//a^x = b mod p
+		double a = Integer.parseInt(args[0]);
+		double b = Integer.parseInt(args[1]);
+		double p = Integer.parseInt(args[2]);
+		
+		
+		
+		s.solve(a,b,p);
+	}
+}
